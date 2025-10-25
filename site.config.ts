f438ccb56aa6d54944e6c483217208fb2bca1663
Testnet Explorer <avidyadan2@gmail.com>
2025-10-25 23:36:00 +0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -23,7 +23,7 @@ export default siteConfig({
   
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: 'https://your-icon-url.com/icon.png',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
   
