7a38f1e4d57667b09b23a3bcc4c269fa6af9d639
Testnet Explorer <avidyadan2@gmail.com>
2025-10-25 22:58:00 +0600

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,42 +2,40 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: '297676c5df6f809bbf66c2b470b70640',
+  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
-
+  
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
+  name: 'Aztec Video Guide - by Xanon',
+  domain: 'aztec-guide-by-xanon.vercel.app',
+  author: 'Xanon XBT',
+  
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
+  description: 'Complete video guide for Aztec Protocol - tutorials, walkthroughs, and educational content',
+  
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-
+  twitter: 'xanonxbt',
+  github: 'xanonxbt',
+  youtube: 'xanonxbt',
+  linkedin: null,
+  
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
-
+  
   // whether or not to enable support for LQIP preview images (optional)
   isPreviewImageSupportEnabled: true,
-
+  
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
   isRedisEnabled: false,
-
+  
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
   // example:
@@ -47,7 +45,7 @@ export default siteConfig({
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
   pageUrlOverrides: null,
-
+  
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   navigationStyle: 'default'
